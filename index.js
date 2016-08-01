143f79bd9e16a6926e7b2e897e175546e7fdd894
OMRS MEH <omrsmeh@gmail.com>
2016-08-01 22:54:40 +0530

    Package update

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 'use strict';
 
-module.exports = require('./lib');
+var CB = require('./lib');
 
-// let CB = require('./lib');
-// let cb = new CB('93E595F6-BD00-47A3-B856-58F76FCE8D79');
-
-// cb.validatePhone('+918127915845', 'sms', (error, response) => {
-//
-//   console.log(error, response);
-// })
-
-//module.exports = require('./lib');
+module.exports = CB.CheckMobi
